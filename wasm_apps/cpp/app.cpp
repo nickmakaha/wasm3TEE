4878bb0bb34648376c16bf48268a706090bd5d90
nickmakaha <nickmakaha@gmail.com>
2024-07-30 13:26:33 -0700

    Add example wasm3 cpp app

---
+++ wasm_apps/cpp/app.cpp
@@ -0,0 +1,9 @@
+#define WASM_EXPORT                   extern "C" __attribute__((used)) __attribute__((visibility ("default")))
+#define WASM_EXPORT_AS(NAME)          WASM_EXPORT __attribute__((export_name(NAME)))
+#define WASM_IMPORT(MODULE,NAME)      __attribute__((import_module(MODULE))) __attribute__((import_name(NAME)))
+#define WASM_CONSTRUCTOR              __attribute__((constructor))
+
+WASM_EXPORT
+void setup() {
+  println("C++ is running!");
+}
