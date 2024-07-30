6e0127e515b07ea6c69f1b3e1536a12e595d7632
nickmakaha <nickmakaha@gmail.com>
2024-07-30 13:30:09 -0700

    Update

--- wasm_apps/cpp/app.cpp
+++ wasm_apps/cpp/app.cpp
@@ -1,8 +1,4 @@
-#define WASM_EXPORT                   extern "C" __attribute__((used)) __attribute__((visibility ("default")))
-#define WASM_EXPORT_AS(NAME)          WASM_EXPORT __attribute__((export_name(NAME)))
-#define WASM_IMPORT(MODULE,NAME)      __attribute__((import_module(MODULE))) __attribute__((import_name(NAME)))
-#define WASM_CONSTRUCTOR              __attribute__((constructor))
-
+#define WASM_EXPORT __attribute__((used)) __attribute__((visibility ("default")))
 WASM_EXPORT
 void setup() {
   println("C++ is running!");
