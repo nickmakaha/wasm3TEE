7590d626fee02e07f2379fe7eb933b0be62613f0
nickmakaha <nickmakaha@gmail.com>
2024-07-30 12:47:57 -0700

    Setup UUID & TA dir

--- optee_examples/wasm3_tee/host/main.c
+++ optee_examples/wasm3_tee/host/main.c
@@ -1,188 +1,106 @@
-// Standard C library headers
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+/*
+ * Copyright (c) 2016, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-// BSD headers
 #include <err.h>
+#include <stdio.h>
+#include <string.h>
 
-// GlobalPlatform Client API
+/* OP-TEE TEE client API (built by optee_client) */
 #include <tee_client_api.h>
 
-// GlobalPlatfrom TA
-#include <wamr_ta.h>
+/* For the UUID (found in the TA's h-file(s)) */
+#include <wasm3_tee_ta.h>
 
-#define timespec_to_micro(t) \
-    t.tv_sec * 1000 * 1000 + t.tv_nsec / 1000
-
-/* TEE resources */
-typedef struct _tee_ctx {
+int main(void)
+{
+	TEEC_Result res;
 	TEEC_Context ctx;
 	TEEC_Session sess;
-    uint8_t *output_buffer;
-    uint64_t output_buffer_size;
-    uint8_t *benchmark_buffer;
-    uint64_t benchmark_buffer_size;
-} tee_ctx;
-
-static void prepare_tee_session(tee_ctx* ctx)
-{
-	TEEC_UUID uuid = TA_WAMR_UUID;
-	uint32_t origin;
-	TEEC_Result res;
+	TEEC_Operation op;
+	TEEC_UUID uuid = TA_WASM3_TEE_UUID;
+	uint32_t err_origin;
 
 	/* Initialize a context connecting us to the TEE */
-	res = TEEC_InitializeContext(NULL, &ctx->ctx);
+	res = TEEC_InitializeContext(NULL, &ctx);
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
 
-	/* Open a session with the TA */
-	res = TEEC_OpenSession(&ctx->ctx, &ctx->sess, &uuid,
-			       TEEC_LOGIN_PUBLIC, NULL, NULL, &origin);
+	/*
+	 * Open a session to the "hello world" TA, the TA will print "hello
+	 * world!" in the log when the session is created.
+	 */
+	res = TEEC_OpenSession(&ctx, &sess, &uuid,
+			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
-			res, origin);
-}
+			res, err_origin);
 
-static void configure_heap_size(tee_ctx *ctx, uint32_t size) {
-    TEEC_Operation op;
-	uint32_t origin;
-	TEEC_Result res;
+	/*
+	 * Execute a function in the TA by invoking it, in this case
+	 * we're incrementing a number.
+	 *
+	 * The value of command ID part and how the parameters are
+	 * interpreted is part of the interface provided by the TA.
+	 */
 
-    memset(&op, 0, sizeof(op));
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE, TEEC_NONE, TEEC_NONE);
-	op.params[0].value.a = size;
-
-	res = TEEC_InvokeCommand(&ctx->sess, COMMAND_CONFIGURE_HEAP, &op, &origin);
-    if (res != TEEC_SUCCESS) {
-        printf("The heap size of WaTZ cannot be configured. Error: %x", res);
-    }
-}
-
-static void allocate_buffers(tee_ctx* ctx, uint64_t buffers_size) {
-    // The output buffer is used to capture writes to stdout from the WASM
-    ctx->output_buffer = malloc(buffers_size);
-    ctx->output_buffer_size = buffers_size;
-
-    // The benchmark buffer is used to capture benchmark information from the TA
-    ctx->benchmark_buffer = malloc(buffers_size);
-    ctx->benchmark_buffer_size = buffers_size;
-}
-
-static bool start_wasm(tee_ctx* ctx, char* wasm_path, char* arg)
-{
-    TEEC_Operation op;
-	uint32_t origin;
-	TEEC_Result res;
-    FILE* wasm_file = NULL;
-    long wasm_file_length;
-    unsigned char *wasm_bytecode;
-
-    //
-    // Dumping the Wasm bytecode
-    //
-    // Open the file in binary mode
-    wasm_file = fopen(wasm_path, "rb");
-    if (wasm_file == NULL)
-    {
-        printf("ERROR: the file %s cannot be opened.\n", wasm_path);
-        return false;
-    }
-    // Jump to the end of the file
-    fseek(wasm_file, 0, SEEK_END);
-    // Get the current byte offset in the file
-    wasm_file_length = ftell(wasm_file);
-    // Allocate the buffer for the bytecode with the size of the file
-    wasm_bytecode = malloc(ftell(wasm_file) * sizeof(unsigned char));
-    // Jump back to the beginning of the file  
-    rewind(wasm_file);
-    // Dump the bytecode into the buffer
-    fread(wasm_bytecode, wasm_file_length, 1, wasm_file);
-    // Close the file
-    fclose(wasm_file);
-
+	/* Clear the TEEC_Operation struct */
 	memset(&op, 0, sizeof(op));
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
-					 TEEC_MEMREF_TEMP_INPUT, TEEC_MEMREF_TEMP_INOUT, TEEC_MEMREF_TEMP_INOUT);
-	op.params[0].tmpref.buffer = wasm_bytecode;
-	op.params[0].tmpref.size = wasm_file_length;
-    op.params[1].tmpref.buffer = arg;
-	op.params[1].tmpref.size = arg != NULL ? strlen(arg) : 0;
-    op.params[2].tmpref.buffer = ctx->output_buffer;
-    op.params[2].tmpref.size = ctx->output_buffer_size;
-    op.params[3].tmpref.buffer = ctx->benchmark_buffer;
-    op.params[3].tmpref.size = ctx->benchmark_buffer_size;
 
-#ifdef PROFILING_LAUNCH_TIME
-    // We start measuring just before the invocation of the code that launches the WASM app in the TA
-    struct timespec time;
-    clock_gettime(CLOCK_MONOTONIC, &time);
-#endif
+	/*
+	 * Prepare the argument. Pass a value in the first parameter,
+	 * the remaining three parameters are unused.
+	 */
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
+					 TEEC_NONE, TEEC_NONE);
+	op.params[0].value.a = 42;
 
-	res = TEEC_InvokeCommand(&ctx->sess, COMMAND_RUN_WASM, &op, &origin);
-    
-#ifdef PROFILING_LAUNCH_TIME
-    // The measurement finished in the WASM app. We are free to take some time to display numbers here.
-    // We print the recorded start at the first column of data. The second one will be printed when
-    // the output buffer is printed.
-    printf("%ld,", timespec_to_micro(time));
-#endif
-
+	/*
+	 * TA_HELLO_WORLD_CMD_INC_VALUE is the actual function in the TA to be
+	 * called.
+	 */
+	printf("Invoking TA to increment %d\n", op.params[0].value.a);
+	res = TEEC_InvokeCommand(&sess, TA_HELLO_WORLD_CMD_INC_VALUE, &op,
+				 &err_origin);
 	if (res != TEEC_SUCCESS)
-    {
-		errx(1, "TEEC_InvokeCommand(START_WASM) failed 0x%x origin 0x%x",
-			res, origin);
-        return false;
-    }
+		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
+			res, err_origin);
+	printf("TA incremented value to %d\n", op.params[0].value.a);
 
-    return true;
-}
+	/*
+	 * We're done with the TA, close the session and
+	 * destroy the context.
+	 *
+	 * The TA will print "Goodbye!" in the log when the
+	 * session is closed.
+	 */
 
-static void terminate_tee_session(tee_ctx* ctx)
-{
-	TEEC_CloseSession(&ctx->sess);
-	TEEC_FinalizeContext(&ctx->ctx);
-}
+	TEEC_CloseSession(&sess);
 
-static void print_buffers(tee_ctx* ctx) {
-    printf("%s%s", ctx->benchmark_buffer, ctx->output_buffer);
-}
+	TEEC_FinalizeContext(&ctx);
 
-static void free_buffers(tee_ctx* ctx) {
-    ctx->output_buffer_size = 0;
-    ctx->benchmark_buffer_size = 0;
-    free(ctx->output_buffer);
-    free(ctx->benchmark_buffer);
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc < 3)
-    {
-        printf("ERROR: The number of arguments does not match.\n");
-        printf("SYNTAX: %s heap_size wasm_path [wasm_arg]\n", argv[0]);
-        exit(1);
-    }
-
-    tee_ctx ctx;
-    bool success = true;
-    uint32_t heap_size = atoi(argv[1]);
-    char* wasm_path = argv[2];
-    char* arg = argc > 3 ? argv[3] : NULL;
-
-    allocate_buffers(&ctx, 5 * 1024);
-
-    prepare_tee_session(&ctx);
-
-    configure_heap_size(&ctx, heap_size);
-  
-    success = start_wasm(&ctx, wasm_path, arg);
-
-    terminate_tee_session(&ctx);
-
-    print_buffers(&ctx);
-    free_buffers(&ctx);
-  
-    return success ? EXIT_SUCCESS : 1;
+	return 0;
 }
---
+++ optee_examples/wasm3_tee/ta/sub.mk
@@ -0,0 +1,5 @@
+global-incdirs-y += include
+srcs-y += wasm3_tee_ta.c
+
+# To remove a certain compiler flag, add a line like this
+#cflags-template_ta.c-y += -Wno-strict-prototypes
